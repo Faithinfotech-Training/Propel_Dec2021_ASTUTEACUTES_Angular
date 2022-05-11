760339f0f3ce27cbd0160f62ffb15683be606203
PL1957 <PL1957@faith.com>
2022-05-11 12:00:22 +0530

    update from Yaseen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { ReceptionistComponent } from './receptionist/receptionist.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { LabtechnicianComponent } from './labtechnician/labtechnician.component';
+import { PharmacistComponent } from './pharmacist/pharmacist.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    ReceptionistComponent,
+    DoctorComponent,
+    LabtechnicianComponent,
+    PharmacistComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
